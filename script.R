5dcda90818ebbe5c83ab2b839a8497c25fdc4198
Shafi Rubbani <shafirubbani@gmail.com>
2019-03-14 13:37:13 -0400

    Initial commit

---
+++ script.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(gt)
+library(janitor)
+
+spain_polls <- read_csv(file = "https://pollofpolls.eu/get/polls/ES-parliament/format/csv") %>% 
+  clean_names() %>% 
+  gather(pp, psoe, bng, cc, erc, pnveaj, cs, vox, podemos, eh_bildu, pacma, p_de_cat, key = "party", value = "vote")
+
+good_firms <- orig %>% 
+  group_by(firm) %>% 
+  summarize(number = n()) %>% 
+  filter(number >= 40) %>% 
+  pull(firm)
+
+orig %>%
+  filter(Sys.Date() - date < 365) %>% 
+  filter(firm %in% good_firms) %>% 
+  ggplot(aes(x = date, y = vote, color = party)) + 
+  geom_smooth()
