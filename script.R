d3553b589b901aac3f1f55532dcc06cbb9fdcf9b
Shafi Rubbani <shafirubbani@gmail.com>
2019-03-14 13:43:02 -0400

    Wrote Rmd file

--- script.R
+++ script.R
@@ -6,13 +6,13 @@ spain_polls <- read_csv(file = "https://pollofpolls.eu/get/polls/ES-parliament/f
   clean_names() %>% 
   gather(pp, psoe, bng, cc, erc, pnveaj, cs, vox, podemos, eh_bildu, pacma, p_de_cat, key = "party", value = "vote")
 
-good_firms <- orig %>% 
+good_firms <- spain_polls %>% 
   group_by(firm) %>% 
   summarize(number = n()) %>% 
   filter(number >= 40) %>% 
   pull(firm)
 
-orig %>%
+spain_polls %>%
   filter(Sys.Date() - date < 365) %>% 
   filter(firm %in% good_firms) %>% 
   ggplot(aes(x = date, y = vote, color = party)) + 
